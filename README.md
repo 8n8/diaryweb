5b4bed2424ee4ab235da4a04edb9b1741bac7405
True Ghiassi <true@ghiassitrio.co.uk>
2023-03-13 21:45:59 +0000

    Revert "Removed the Elm app"
    This reverts commit 7c4f5b56ac926fa8648e2416a9efdd4b8fda6220.
    
    I've been experimenting with constructing the HTML on the server, but
    it's not as simple as I had hoped.

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # diaryweb
 
-*Work in progress*
+** Work in progress **
 
-This is a small web app for writing a personal diary.
+This is a web app for writing a diary.
+
+## Build for development
+
+It's not deployable yet, so these are development build instructions.
+
+### Frontend
+
+The frontend is written in Elm. Build as follows:
+
+```
+cd frontend
+elm make src/Main.elm --output=/dev/null
+```
+
+### Backend
+
+The backend is written in Haskell. Install the Haskell Tool Stack and then build as follows:
+
+```
+cd backend
+stack build
+```
---
+++ frontend/src/Main.elm
@@ -0,0 +1,384 @@
+module Main exposing (main)
+
+import AccessCode exposing (AccessCode)
+import Browser exposing (Document)
+import Element exposing (Element)
+import Element.Font as Font
+import Indicator
+import Element.Input as Input
+import Element.Region as Region
+import Html
+import Row exposing (Row)
+import Rows exposing (Rows)
+import Http
+import Task
+import Time exposing (Month, Posix, Zone)
+import Bytes exposing (Bytes)
+import Base64
+
+
+type Model
+    = FatalError String
+    | GettingTimeZone
+    | Ok OkModel
+
+
+type alias OkModel =
+    { accessCodeBox : Maybe Bytes
+    , diaryEntryBox : String
+    , rows : Loadable Rows
+    , zone : Zone
+    }
+
+
+type Loadable a
+    = Loading
+    | Loaded a
+    | NoSuchTable
+    | NotAsked
+
+
+type Msg
+    = AccessCode String
+    | DiaryEntry String
+    | TimeZone Zone
+    | SubmitAccessCode Bytes
+
+
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case model of
+        FatalError _ ->
+            ( model, Cmd.none )
+
+        GettingTimeZone ->
+            updateGettingTimeZone msg
+
+        Ok okModel ->
+            updateOk msg okModel
+
+
+updateGettingTimeZone : Msg -> ( Model, Cmd Msg )
+updateGettingTimeZone msg =
+    case msg of
+        AccessCode _ ->
+            ( GettingTimeZone, Cmd.none )
+
+        DiaryEntry _ ->
+            ( GettingTimeZone, Cmd.none )
+
+        TimeZone zone ->
+            ( { accessCodeBox = Nothing
+              , diaryEntryBox = ""
+              , rows = NotAsked
+              , zone = zone
+              }
+                |> Ok
+            , Cmd.none
+            )
+
+        SubmitAccessCode _ ->
+            ( GettingTimeZone, Cmd.none )
+
+
+updateOk : Msg -> OkModel -> ( Model, Cmd Msg )
+updateOk msg model =
+    case msg of
+        AccessCode accessCode ->
+            case Base64.toBytes accessCode of
+                Nothing ->
+                    (Ok model, Cmd.none)
+
+                Just bytes ->
+                    ( Ok { model | accessCodeBox = Just bytes }, Cmd.none )
+
+        DiaryEntry diaryEntry ->
+            ( Ok { model | diaryEntryBox = diaryEntry }, Cmd.none )
+
+        TimeZone zone ->
+            ( Ok { model | zone = zone }, Cmd.none )
+
+        SubmitAccessCode accessCode ->
+            ( Ok { model | rows = Loading }
+            , { url = "/api"
+                , body =
+                    Http.bytesBody
+                        "application/octet-stream"
+                        ([ Encode.unsignedInt8 Indicator.get
+                         , Encode.bytes accessCode
+                         ]
+                            |> Encode.sequence
+                            |> Encode.encode)
+                , expect = Http.expectBytes GotTable decodeGotTable
+                }
+                    |> Http.post
+            )
+
+
+decodeGotTable : Decoder (List TableRow)
+decodeGotTable =
+    Decode.unsignedInt8
+        |> Decode.andThen (\indicator ->
+            if indicator == Indicator.got then
+                decodeTableRows
+
+            else
+                Decode.fail
+    Decode.map2 (\indicator tableRows ->
+        tableRows)
+        (Decode.unsignedInt8 
+        
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Diary"
+    , body =
+        [ Element.layout
+            [ Font.family [ Font.sansSerif ] ]
+            (viewElement model)
+        ]
+    }
+
+
+viewElement : Model -> Element Msg
+viewElement model =
+    case model of
+        FatalError errorMessage ->
+            viewFatalError errorMessage
+
+        Ok okModel ->
+            viewOk okModel
+
+        GettingTimeZone ->
+            Element.none
+
+
+viewOk : OkModel -> Element Msg
+viewOk { accessCodeBox, diaryEntryBox, rows, zone } =
+    [ viewTopBar accessCodeBox
+    , viewDiaryEntryBox diaryEntryBox
+    , case rows of
+        Loaded loaded ->
+           viewDiary zone loaded
+    ]
+        |> Element.column []
+
+
+viewDiary : Zone -> Rows -> Element msg
+viewDiary zone rows =
+    Rows.toList rows
+        |> List.map (viewRow zone)
+        |> Element.column [ Element.spacing 24 ]
+
+
+viewRow : Zone -> Row -> Element msg
+viewRow zone row =
+    case row of
+        Row.NotFound ->
+            Element.none
+
+        Row.Loaded time entry ->
+            viewLoaded zone time entry
+
+        Row.Loading ->
+            Element.text "Loading..."
+
+
+viewLoaded : Zone -> Posix -> String -> Element msg
+viewLoaded zone time text =
+    [ viewTimestamp zone time
+    , viewDiaryEntryText text
+    ]
+        |> Element.paragraph []
+
+
+viewDiaryEntryText : String -> Element msg
+viewDiaryEntryText text =
+    Element.text text
+
+
+viewTimestamp : Zone -> Posix -> Element msg
+viewTimestamp zone time =
+    [ viewHourMinute (Time.toHour zone time) (Time.toMinute zone time)
+    , " "
+    , viewDayName (Time.toWeekday zone time)
+    , " "
+    , String.fromInt (Time.toDay zone time)
+    , " "
+    , viewMonth (Time.toMonth zone time)
+    , " "
+    , String.fromInt (Time.toYear zone time)
+    ]
+        |> String.concat
+        |> Element.text
+
+
+viewHourMinute : Int -> Int -> String
+viewHourMinute hour minute =
+    viewHour hour ++ ":" ++ viewMinute minute ++ " " ++ viewAmPm hour
+
+
+viewHour : Int -> String
+viewHour hour =
+    String.fromInt (modBy 12 hour)
+
+
+viewMinute : Int -> String
+viewMinute minute =
+    String.padLeft 2 '0' (String.fromInt minute)
+
+
+viewAmPm : Int -> String
+viewAmPm hour =
+    if hour <= 12 then
+        "am"
+
+    else
+        "pm"
+
+
+viewDayName : Time.Weekday -> String
+viewDayName weekday =
+    case weekday of
+        Time.Mon ->
+            "Mon"
+
+        Time.Tue ->
+            "Tue"
+
+        Time.Wed ->
+            "Wed"
+
+        Time.Thu ->
+            "Thu"
+
+        Time.Fri ->
+            "Fri"
+
+        Time.Sat ->
+            "Sat"
+
+        Time.Sun ->
+            "Sun"
+
+
+viewMonth : Month -> String
+viewMonth month =
+    case month of
+        Time.Jan ->
+            "Jan"
+
+        Time.Feb ->
+            "Feb"
+
+        Time.Mar ->
+            "Mar"
+
+        Time.Apr ->
+            "Apr"
+
+        Time.May ->
+            "May"
+
+        Time.Jun ->
+            "Jun"
+
+        Time.Jul ->
+            "Jul"
+
+        Time.Aug ->
+            "Aug"
+
+        Time.Sep ->
+            "Sep"
+
+        Time.Oct ->
+            "Oct"
+
+        Time.Nov ->
+            "Nov"
+
+        Time.Dec ->
+            "Dec"
+
+
+viewDiaryEntryBox : String -> Element Msg
+viewDiaryEntryBox diaryEntryBox =
+    Input.multiline
+        []
+        { onChange = DiaryEntry
+        , text = diaryEntryBox
+        , placeholder = Nothing
+        , label = Input.labelAbove [] (Element.text "Write a diary entry:")
+        , spellcheck = True
+        }
+
+
+viewTopBar : Maybe Bytes -> Element Msg
+viewTopBar accessCodeBox =
+    [ Input.text
+        []
+        { onChange = AccessCode
+        , text =
+            case accessCodeBox of
+                Nothing -> ""
+                Just bytes ->
+                    Base64.fromBytes bytes |> Maybe.withDefault ""
+        , placeholder = Nothing
+        , label = Input.labelLeft [] (Element.text "Access code:")
+        }
+    , Input.button
+        []
+        { onPress = Maybe.map SubmitAccessCode accessCodeBox
+        , label = Element.text "Submit"
+        }
+    ]
+        |> Element.row []
+
+
+viewFatalError : String -> Element msg
+viewFatalError errorMessage =
+    [ heading1 "Sorry, something went wrong"
+    , paragraph "There was an internal error. This is as much as is known:"
+    , monospace errorMessage
+    ]
+        |> Element.column [ Element.spacing 16 ]
+
+
+heading1 : String -> Element msg
+heading1 contents =
+    Element.el [ Region.heading 1, Font.size 32 ] (Element.text contents)
+
+
+paragraph : String -> Element msg
+paragraph contents =
+    Element.paragraph [ Font.size 16 ] [ Element.text contents ]
+
+
+monospace : String -> Element msg
+monospace contents =
+    Element.text contents
+        |> Element.el
+            [ Font.size 16
+            , Font.family [ Font.monospace ]
+            ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( GettingTimeZone, Task.perform TimeZone Time.here )
